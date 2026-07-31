246970b441a6d4a4ab2acc43f50ab74f34f8772d
SHUBHAM <shubham@SHUBHAMs-MacBook.local>
2026-07-31 16:21:28 +0530

    UI issue fixed

--- lib/features/home/hero_section.dart
+++ lib/features/home/hero_section.dart
@@ -18,7 +18,7 @@ class HeroSection extends StatelessWidget {
     final size = MediaQuery.of(context).size;
 
     return SizedBox(
-      height: size.height * 1.3,
+      height: size.height * 1.45,
       child: Center(
         child: SingleChildScrollView(
           padding: const EdgeInsets.only(left: 32, right: 32, top: 100),
